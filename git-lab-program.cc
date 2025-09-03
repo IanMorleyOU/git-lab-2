6e433315a41b574d2f9febe024db2ec19f14d202
IanMorleyOU <im787824@ohio.edu>
2025-09-03 17:46:24 -0400

    C++ File

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ian Morley
+ *        Date: 9/3/2025
+ * Description: C++ program for the 2nd lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
